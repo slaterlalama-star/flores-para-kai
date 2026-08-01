56944fafecb431fb5995a7af04a140b6f6e25d5e
slaterlalama-star <slaterlalama@gmail.com>
2026-08-01 18:21:29 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,42 @@
 const frases = [
-
-"Hoy muchas personas regalan flores.",
-
-"Yo también quería hacerlo...",
-
-"...pero quería darte unas que nunca se marchiten."
-
+  "Hoy muchas personas regalan flores.",
+  "Yo también quería hacerlo...",
+  "...pero quería darte unas que nunca se marchiten."
 ];
 
 let i = 0;
 
 const mensaje = document.getElementById("mensaje");
+const final = document.getElementById("final");
 
-setInterval(()=>{
+const intervalo = setInterval(() => {
 
-i++;
+    i++;
 
-if(i<frases.length){
+    if(i < frases.length){
 
-mensaje.style.opacity=0;
+        mensaje.style.opacity = 0;
 
-setTimeout(()=>{
+        setTimeout(()=>{
 
-mensaje.innerHTML=frases[i];
+            mensaje.innerHTML = frases[i];
+            mensaje.style.opacity = 1;
 
-mensaje.style.opacity=1;
+        },700);
 
-},700);
+    }else{
 
-}
+        clearInterval(intervalo);
+
+        mensaje.style.opacity = 0;
+
+        setTimeout(()=>{
+
+            mensaje.style.display="none";
+            final.style.display="block";
+
+        },1000);
+
+    }
 
 },3500);
